047f6ad8e13c70e4ed9e76a15439c0c4198fa76f
DevAnsH1530 <devanshgupta1947@gmail.com>
2024-08-01 23:14:00 +0530

    Learned some new things in React and also learned about props and states in React

--- React_Vite_Learning/Hooks Learning/src/App.jsx
+++ React_Vite_Learning/Hooks Learning/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Scroll from './Scroll'
 import './App.css'
 
 function App() {
@@ -8,25 +9,26 @@ function App() {
 
   return (
     <>
-      <h1>Counter = {count}</h1>
-      <h2>creating a project where COUNTER MUST BE GREATER THAN OR EQUAL TO 0</h2>
-      <div className="card">
-        <button onClick={()=>{
-          setCount(count+1);
-        }}>
-          Add value
-        </button>
-        <button onClick={()=>{
-          if(count>=1){
-            count--;
-            setCount(count);
-          }
-        }}>
-          decrease value
-        </button>
-        {/* <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
+      <div className='bg-green-400 rounded-2xl'>
+        <h1>Counter = {count}</h1>
+        <div className='snap-x w-1/12 overflow-hidden'> 
+        <h2 className='snap-center'>creating a project where COUNTER MUST BE GREATER THAN OR EQUAL TO 0</h2>
+        </div>
+        <div className="card">
+          <button onClick={ ()=> setCount(++count)} className='bg-white rounded-xl'>
+            Add value
+          </button>
+          <button onClick={()=>{
+            if(count>=1){
+              // count--;
+              setCount(--count);
+            }
+          }} className='bg-white rounded-xl hover:bg-blue-400 shadow-lg transition-transform'>
+            decrease value
+          </button>
+        </div>
+        <Scroll/>
+        
       </div>
     </>
   )
